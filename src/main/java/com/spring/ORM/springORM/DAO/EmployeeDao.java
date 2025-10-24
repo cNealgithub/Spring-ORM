45a5534fb1d461bb4558b78115848e5b0234f9da
Neal Chakravarty <nealchakravarty930@gmail.com>
2025-10-24 20:57:48 +0530

    CRUD features mod

--- src/main/java/com/spring/ORM/springORM/DAO/EmployeeDao.java
+++ src/main/java/com/spring/ORM/springORM/DAO/EmployeeDao.java
@@ -1,9 +1,17 @@
 package com.spring.ORM.springORM.DAO;
 
+import java.util.List;
+
 import com.spring.ORM.springORM.Entites.Employee;
 
 public interface EmployeeDao {
 
 	public int insert(Employee emp);
-
+	public Employee getEmployee(int id);
+	//to get all employees
+	public List<Employee>allEmployees();
+	//delete method
+	public void deleteEmployee(int id); 
+	//update method
+	public void updateEmployee(Employee emp);
 }
