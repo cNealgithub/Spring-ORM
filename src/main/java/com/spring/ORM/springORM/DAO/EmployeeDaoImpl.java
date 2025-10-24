a7ea99414d3e097d429d917c945962a1fbc6258a
Neal Chakravarty <nealchakravarty930@gmail.com>
2025-10-24 20:53:11 +0530

    CRUD features

--- src/main/java/com/spring/ORM/springORM/DAO/EmployeeDaoImpl.java
+++ src/main/java/com/spring/ORM/springORM/DAO/EmployeeDaoImpl.java
@@ -1,5 +1,7 @@
 package com.spring.ORM.springORM.DAO;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -17,7 +19,26 @@ public class EmployeeDaoImpl implements EmployeeDao{
 		int i = (int)this.hibernateTemplate.save(emp);
 		return i;
 	}
-
+	//to get single employee
+	public Employee getEmployee(int id) {
+		Employee emp = this.hibernateTemplate.get(Employee.class, id);
+		return emp;
+	}
+	//to get all employees
+	public List<Employee>allEmployees(){
+		List<Employee> all = this.hibernateTemplate.loadAll(Employee.class);
+		return all;
+	}
+	//delete method
+	public void deleteEmployee(int id) {
+		Employee emp = this.hibernateTemplate.get(Employee.class, id);
+		this.hibernateTemplate.delete(emp);
+		System.out.println("Deleted id ; " + id);
+	}
+	//update method
+	public void updateEmployee(Employee emp) {
+		this.hibernateTemplate.update(emp);
+	}
 	public HibernateTemplate getHibernateTemplate() {
 		return hibernateTemplate;
 	}
